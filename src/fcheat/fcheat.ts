0420fd9ea1869f5e9f865cc9c76d30d962d9c40d
Adam <i@adamurban.de>
2019-12-21 20:35:11 +0100

    :sparkles:

--- src/fcheat/fcheat.ts
+++ src/fcheat/fcheat.ts
@@ -54,13 +54,16 @@ export class FCheat {
   private find(cheat: Cheat, input: string) {
     if (!input.includes(" ")) {
       return (
-        cheat.title.includes(input) ||
-        (cheat.tags && cheat.tags.includes(input))
+        cheat.title.startsWith(input) ||
+        (cheat.tags && cheat.tags.find(t => t.startsWith(input)))
       );
     }
     const splitted = input.split(" ");
     const found = splitted.map(i => {
-      return cheat.title.includes(i) || (cheat.tags && cheat.tags.includes(i));
+      return (
+        cheat.title.startsWith(i) ||
+        (cheat.tags && cheat.tags.find(t => t.startsWith(i)))
+      );
     });
     return found.every(f => f === true);
   }
